e43c45596ee7ded65e22f8bb01acf31bce481094
Kamil Rzeszutek <kamilrzeszutek@Kamils-MacBook-Pro.local>
2020-02-04 15:23:11 +0100

    Added Correct Hole

--- myCode.js
+++ myCode.js
@@ -27,5 +27,15 @@ function createHole() {
         holes.push(hole);
         container.appendChild(hole);
     }
+    randomGoodHole(1);
 }
+ 
 createHole();
+ function randomGoodHole(i){                                 // Dodanie dobrej dziury
+     let goodHole = Math.floor(Math.random()*holes.length);
+     if(goodHole ==i&&i<holes.length){i++;}                  // uniknięcie pojawienia się dobrej dziury w tym samym miejscu
+     else{i--;}
+     holes[goodHole].classList.remove("hole");
+     holes[goodHole].classList.add("correctHole")
+ }                                                           
+
