301e6e2a8e9f6724ea0d0bdeef6e18fb2037c002
Kamil Rzeszutek <kamilrzeszutek@Kamils-MacBook-Pro.local>
2020-02-04 03:58:48 +0100

    spawnHole

--- myCode.js
+++ myCode.js
@@ -5,4 +5,24 @@ var posX = 30;
 var posY = 30;
 var startGame = 0;
 var ball = document.getElementById("ball");
-var container = document.getElementById("container");
+let container = document.getElementsByClassName("container")[0];
+
+function createHole() {
+    for(i=2;i<(window.innerWidth/100);i++){
+        let hole = document.createElement('div');
+        hole.classList.add("hole");
+        hole.style.left=100*i+Math.random()*75-95+'px';
+        hole.style.top=Math.random()*(window.innerHeight-95)/2+'px';
+        holes.push(hole);
+        container.appendChild(hole);
+    }
+    for(i=2;i<(window.innerWidth/100);i++){
+        let hole = document.createElement('div');
+        hole.classList.add("hole");
+        hole.style.left=100*i+Math.random()*75-95+'px';
+        hole.style.top=Math.random()*(window.innerHeight)/2+window.innerHeight/2-100+'px';
+        holes.push(hole);
+        container.appendChild(hole);
+    }
+}
+createHole();
